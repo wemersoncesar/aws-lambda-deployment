404ad3c2ec323d52dcb900834f69392534f9ff4f
Wemerson Cesar <wemersoncesar@LINKIT-MB0313.local>
2024-03-29 15:15:14 +0100

    first commit

---
+++ main_new.py
@@ -0,0 +1,10 @@
+"""
+This module provides the implementation of the `lambda_handler` function executed by the AWS Lambda function in charge
+of executing prophet pipelines on new preprocessed data.
+"""
+
+
+def lambda_handler(event, context):
+    print(">>>>> ok <<<<<<")
+
+    
