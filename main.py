8f3f69cc4e79b78112919b29f43c824694ef5110
wemersoncesar <wemersoncesar@gmail.com>
2024-05-31 14:21:40 +0200

    add app

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ This module provides the implementation of the `lambda_handler` function execute
 of executing prophet pipelines on new preprocessed data.
 """
 
+from app.config import Config
+import app.S3Cleaner as S3Cleaner
+from app.emr import TransientEMRLauncher, AtaSteps, GenericPipelineStep
+from submit_args import SubmitArgs
+import os
 
 def lambda_handler(event, context):
     print(">>>>> ok <<<<<<")
-
+    cleaner = S3Cleaner(config)
+    cleaner.clean_checkpoint_folder()
     
